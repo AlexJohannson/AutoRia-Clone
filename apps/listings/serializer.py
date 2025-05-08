3e835ec848594a360e336522ae69fcd86c3b63cb
AlexJohannson <mehedsanya@gmail.com>
2025-05-08 20:13:27 +0200

    [AutoRia_Clone] - update

--- apps/listings/serializer.py
+++ apps/listings/serializer.py
@@ -1,35 +1,3 @@
-# from rest_framework import serializers
-#
-# from ..car_brand.models import CarBrandModel
-# from ..car_brand.serializer import CarBrandSerializer
-# from .models import ListingSellersModel
-#
-#
-# class ListingSerializer(serializers.ModelSerializer):
-#     brand = CarBrandSerializer(read_only=True)
-#
-#
-#
-#     class Meta:
-#         model = ListingSellersModel
-#         fields = (
-#             'id',
-#             'brand',
-#             'model',
-#             'year',
-#             'country',
-#             'region',
-#             'city',
-#             'price',
-#             'seller',
-#             'views',
-#             'daily_views',
-#             'weekly_views',
-#             'monthly_views',
-#             'last_view_date'
-#         )
-
-
 from rest_framework import serializers
 
 from ..car_brand.models import CarBrandModel
@@ -63,7 +31,8 @@ class ListingSerializer(serializers.ModelSerializer):
             'last_view_date'
         )
 
+
+
     def create(self, validated_data):
-        brand = validated_data.pop('brand_id')
-        validated_data['brand'] = brand
+        validated_data['brand'] = validated_data.pop('brand_id')
         return super().create(validated_data)
